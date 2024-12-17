fa5aa649fe0b7630112a82fee93fab23240fdf7e
Gurjot Saini <gurjot.saini129@gmail.com>
2024-12-17 18:34:13 -0500

    Adds conditional rendering to ListGroup component

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -2,9 +2,15 @@ function ListGroup() {
   //list of places
   const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
+  const getMessage = () => {
+    return items.length === 0 ? <p>No items found</p> : null;
+  };
+
   return (
     <>
       <h1>List</h1>
+
+      {getMessage()}
 
       <ul className="list-group">
         {items.map((item) => (
