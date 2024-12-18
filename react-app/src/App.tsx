aadb19daad01be8001e2d53f1de6097df27b301c
Gurjot Saini <gurjot.saini129@gmail.com>
2024-12-17 21:29:14 -0500

    Replaces Listgroup component with the Alert component in order to demonstrate that HTML children can be passed to a component

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,21 +1,11 @@
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  //list of cities
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-
-  const handleItemSelection = (item: string) => {
-    console.log(item);
-  };
-
   return (
-    <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleItemSelection}
-      />
-    </div>
+    <Alert>
+      Hello <h2>World</h2>
+    </Alert>
   );
 }
 
